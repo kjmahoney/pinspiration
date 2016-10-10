a59ec0e9b2eb026df97ae4f384c0752bbc8dbcbe
Kevin Mahoney <kjmahoney13@gmail.com>
2016-10-10 12:52:20 -0400

    added form

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,8 +7,20 @@ class PinsController < ApplicationController
   end
 
   def new
+    @pin = Pin.new
   end
 
   def show
   end
+
+  def create
+    @pin = Pin.create(pin_params)
+
+    redirect_to pins_path
+  end
+
+  private
+    def  pin_params
+      params.require(:pin).permit(:title,:img_url)
+    end
 end
